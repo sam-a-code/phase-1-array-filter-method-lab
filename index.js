de820e5bd97e72d973f89ad20a25251c63887a8d
Sam Avery <sam.avery1@gmail.com>
2022-09-07 11:48:04 -0500

    commit tree 5c3ca7cfc966c948d0330f0df2d451c910efe8c1

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+
+function findMatching(drivers, string){
+    return drivers.filter(drivers => {return drivers.toLowerCase() === string.toLowerCase()
+})
+}
+
+function fuzzyMatch(drivers, name){
+    return drivers.filter(
+        function (driver) {
+            return driver.toLowerCase().substring(0, name.length) === name.toLowerCase()})
+}
+
+function matchName(driver, nameProperty){
+    return driver.filter(name => name.name === nameProperty)
+}
